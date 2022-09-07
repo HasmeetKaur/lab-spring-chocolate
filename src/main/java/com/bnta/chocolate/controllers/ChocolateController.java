57534e47f09d5fc9c7cb3a3a0e0469e658d7d4d3
Hasmeet <hasmeetkaur.hk@gmail.com>
2022-09-07 16:00:49 +0100

    attempted mvp3

--- src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
+++ src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
@@ -1,4 +1,39 @@
 package com.bnta.chocolate.controllers;
 
+import com.bnta.chocolate.models.Chocolate;
+import com.bnta.chocolate.models.Estate;
+import com.bnta.chocolate.services.ChocolateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/chocolates")
 public class ChocolateController {
+
+    @Autowired
+    ChocolateService chocolateService;
+
+    @GetMapping
+    public ResponseEntity<List<Chocolate>> getAllChocolates(){
+        List<Chocolate> chocolates = chocolateService.getAllChocolates();
+        return new ResponseEntity<>(chocolates, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Chocolate> getChocolateById(@PathVariable Long id){
+        Optional<Chocolate> chocolate = chocolateService.getChocolateById(id);
+        if(chocolate.isPresent()){
+            return new ResponseEntity<>(chocolate.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
 }
